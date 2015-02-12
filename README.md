8f876e4edffdced5982554a848af314e92fb08e7
litaotao <charles.upboy@gmail.com>
2015-02-12 17:46:47 +0800

    add two file

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# SourceCodeReading - just for fun and skill-promoting.
+# SourceCodeReading
+    Just for fun and skill-promoting.
 
 Enjoy a piece of source code every week.
 ====
 
-- No.1 Feb.9th.2015-Feb.15th.2015 SocketServer.py Python
+| Weekly | Code  |  Language | 
+|---|---|---|
+| Feb.9th.2015-Feb.15th.2015 | SocketServer.py | Python |
+| Feb.9th.2015-Feb.15th.2015 | select.py | Python |
+
 
 
---
+++ select.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+#
+# Copyright 2012 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Select-based IOLoop implementation.
+
+Used as a fallback for systems that don't support epoll or kqueue.
+"""
+from __future__ import absolute_import, division, print_function, with_statement
+
+import select
+
+from tornado.ioloop import IOLoop, PollIOLoop
+
+'''
+# 在python源码里居然没有找到select.py文件，我就先找了个类似的来学习学习，
+  tornado下的：/root/anaconda/lib/python2.7/site-packages/tornado/platform/select.py
+'''
+
+'''
+# 果然是用set来做数据格式的。
+'''
+class _Select(object):
+    """A simple, select()-based IOLoop implementation for non-Linux systems"""
+    def __init__(self):
+        self.read_fds = set()
+        self.write_fds = set()
+        self.error_fds = set()
+        self.fd_sets = (self.read_fds, self.write_fds, self.error_fds)
+
+    def close(self):
+        pass
+'''
+# 在ERROR的情况下，为何要注册read_fds呢，这里需要思考一下。
+'''
+    def register(self, fd, events):
+        if fd in self.read_fds or fd in self.write_fds or fd in self.error_fds:
+            raise IOError("fd %s already registered" % fd)
+        if events & IOLoop.READ:
+            self.read_fds.add(fd)
+        if events & IOLoop.WRITE:
+            self.write_fds.add(fd)
+        if events & IOLoop.ERROR:
+            self.error_fds.add(fd)
+            # Closed connections are reported as errors by epoll and kqueue,
+            # but as zero-byte reads by select, so when errors are requested
+            # we need to listen for both read and error.
+            self.read_fds.add(fd)
+
+    def modify(self, fd, events):
+        self.unregister(fd)
+        self.register(fd, events)
+'''
+# discard in set:　Remove an element from a set if it is a member.
+  If the element is not a member, do nothing.
+'''
+    def unregister(self, fd):
+        self.read_fds.discard(fd)
+        self.write_fds.discard(fd)
+        self.error_fds.discard(fd)
+
+    def poll(self, timeout):
+        readable, writeable, errors = select.select(
+            self.read_fds, self.write_fds, self.error_fds, timeout)
+        events = {}
+        for fd in readable:
+            events[fd] = events.get(fd, 0) | IOLoop.READ
+        for fd in writeable:
+            events[fd] = events.get(fd, 0) | IOLoop.WRITE
+        for fd in errors:
+            events[fd] = events.get(fd, 0) | IOLoop.ERROR
+        return events.items()
+
+
+class SelectIOLoop(PollIOLoop):
+    def initialize(self, **kwargs):
+        super(SelectIOLoop, self).initialize(impl=_Select(), **kwargs)
